c43918252c883bab6f11341231ca7b4bb37873d3
dadina01 <83963798+dadina01@users.noreply.github.com>
2021-05-12 08:46:58 +0200

    Add files via upload
    First commit

---
+++ cale/evo.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Agenda</title>
+
+
+
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="./evo-calendar/css/evo-calendar.min.css">
+    <link rel="stylesheet" type="text/css" href="./evo-calendar/css/evo-calendar.orange-coral.min.css">
+    <link rel="stylesheet" type="text/css" href="./evo-calendar/css/evo-calendar.midnight-blue.min.css">
+    <link rel="stylesheet" type="text/css" href="./evo-calendar/css/evo-calendar.royal-navy.min.css">
+
+    <link rel="stylesheet" type="text/css" href="./demo/demo.css">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Mono&display=swap" rel="stylesheet">
+
+
+</head>
+<body>
+	<?php 
+
+<div class="hero">
+	
+<div id="calendar"></div>
+
+</div>
+
+
+ <script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js"></script>
+
+
+<script type="text/javascript">
+	
+	$(document).ready(function(){
+		$('#calendar').evoCalendar({
+
+		})
+	})
+</script>
+
+?>
+</body>
+</html>
